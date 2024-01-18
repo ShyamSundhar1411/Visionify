474e28786280350d7e889bcccf1b4fd547df5194
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-01-18 23:58:20 +0530

    fix: updated handle and predict

--- visionify-frontend/src/components/ImageUploadComponent.jsx
+++ visionify-frontend/src/components/ImageUploadComponent.jsx
@@ -1,23 +1,37 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import axios from "axios";
+import { styled } from '@mui/material/styles';
+import Button from "@mui/material/Button";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import LinearProgress from '@mui/material/LinearProgress';
 
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 const ImageUpload = () => {
   const [file, setFile] = useState(null);
   const [prediction, setPrediction] = useState(null);
   const [loading, setLoading] = useState(false);
+
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
-  console.log("Hey Form");
+
   const handleUploadAndPredict = async (event) => {
     event.preventDefault();
-    console.log("Hey Pressed")
     try {
       setLoading(true);
       const formData = new FormData();
       formData.append("file", file);
-      const response = await axios.post("http://127.0.0.1:8000/predict/cifar", formData, {
+      const response = await axios.post("http://127.0.0.1:8000/api/v1/auth/token/", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -26,18 +40,20 @@ const ImageUpload = () => {
       setPrediction(response.data.predicted_class);
     } catch (error) {
       console.error("Error uploading and predicting:", error);
-    }finally{
+    } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div>
-      <form>
-        <input type="file" accept= ".jpeg, .png, .jpg" onChange={handleFileChange} />
-        <button onClick={handleUploadAndPredict}>Upload and Predict</button>
-      </form>
-      {loading && <p>Loading...</p>}
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "16px" }}>
+    
+      <Button onChange={handleFileChange} onSubmit={handleUploadAndPredict} component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+        Upload file
+        <VisuallyHiddenInput type="file" />
+      </Button>
+
+      {loading && <LinearProgress style={{ width: "100%", marginTop: "16px" }} />}
       {prediction !== null && !loading && <p>Predicted Class: {prediction}</p>}
     </div>
   );
